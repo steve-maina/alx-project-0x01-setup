de7115ba8c568eb1794aee4e9f9ed6a41da5870b
Steve Maina <stevemainam@gmail.com>
2025-07-20 18:00:52 +0300

    modify interfaces/index.ts, pages/posts/index.tsx, components/common/PostModal.tsx

--- alx-project-0x01/interfaces/index.ts
+++ alx-project-0x01/interfaces/index.ts
@@ -4,6 +4,17 @@ export interface PostProps {
   title: string;
   body: string;
 }
+export interface PostData {
+  userId: number;
+  id?: number;
+  title: string;
+  body: string;
+}
+
+export interface PostModalProps {
+  onClose: () => void;
+  onSubmit: (post: PostData) => void;
+}
 
 export interface UserProps {
   id: number;
