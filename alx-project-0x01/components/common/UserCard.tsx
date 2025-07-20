6ba2a24c6207980ae2c7de2b2c898c4ed91de6fe
Steve Maina <stevemainam@gmail.com>
2025-07-20 17:47:52 +0300

    modify interfaces/index.tsx, pages/users/index.tsx, components/common/UserCard.tsx

---
+++ alx-project-0x01/components/common/UserCard.tsx
@@ -0,0 +1,17 @@
+import { UserProps } from "@/interfaces";
+
+const UserCard: React.FC<UserProps> = ({ name, email, id }) => {
+  return (
+    <div className="max-w-xl mx-auto my-6 p-6 bg-white rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300">
+      <div className="mb-4">
+        <h2 className="text-2xl font-semibold text-gray-800">{name}</h2>
+      </div>
+      <p className="text-gray-600">{email}</p>
+      <div className="mt-4 flex items-center justify-between text-sm text-gray-500">
+        <span>User ID: {id}</span>
+      </div>
+    </div>
+  );
+};
+
+export default UserCard;
--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -1,11 +1,54 @@
+import UserCard from "@/components/common/UserCard";
 import Header from "@/components/layout/Header";
+import { UserProps } from "@/interfaces";
 
-const Users: React.FC = () => {
+const Users: React.FC = ({ posts }) => {
   return (
     <div className="flex flex-col h-screen">
       <Header />
+
+      <div className="grid grid-cols-3 gap-2 ">
+        {posts?.map(
+          (
+            {
+              name,
+              email,
+              id,
+              username,
+              address,
+              phone,
+              company,
+              website,
+            }: UserProps,
+            key: number
+          ) => (
+            <UserCard
+              id={id}
+              key={key}
+              name={name}
+              email={email}
+              username={username}
+              address={address}
+              phone={phone}
+              company={company}
+              website={website}
+            />
+          )
+        )}
+      </div>
     </div>
   );
 };
+
+export async function getStaticProps() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  const posts = await response.json();
+
+  return {
+    props: {
+      posts,
+    },
+  };
+}
 
 export default Users;
